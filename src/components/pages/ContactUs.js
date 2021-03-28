432db3c69ffe2a79482eaa2dac19f17b19a78224
bijay60 <Thapathali1!>
2021-03-28 17:31:18 -0400

    Testing s3

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function ContactUs() {
-  return <h1 className='contact-us'>CONTACT</h1>;
+  return <h1 className='contact-us'>CONTACTO</h1>;
 }
